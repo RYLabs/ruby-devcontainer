7ea2a9d738acf87b0936d3f122de1358edff529a
Calvin Yu <calvin@rylabs.io>
2021-09-06 16:08:39 +0000

    Ruby devcontainer setup w/ RVM mixed mode

---
+++ rvm_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -xe
+
+USERNAME=$2
+
+mkdir -p /home/${USERNAME}/data/rvm
+
+echo 'Files here are expected to be mounted across containers' > /home/${USERNAME}/data/README
+
+# Symlink RVM directory to ~/data so it persists across container restarts
+sudo -u ${USERNAME} ln -nfs /home/${USERNAME}/data/rvm /home/${USERNAME}/.rvm
+chown ${USERNAME}:${USERNAME} -R /home/${USERNAME}/data
+
+# Enable RVM mixed mode so gems are stored in ~/.rvm (actually, ~/data/rvm)
+sudo -u ${USERNAME} bash --login -c 'rvm user gemsets'
